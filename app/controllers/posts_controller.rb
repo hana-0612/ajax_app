236c27736e5687057b31f7385e05a09f15520ae4
hana-0612 <hyania1206@gmail.com>
2021-08-01 16:12:07 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
@@ -9,7 +8,7 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
   end
 end
